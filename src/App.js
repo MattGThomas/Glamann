9dfe4524631ca8ce8b40ba472707ec4e07487cd3
MattGThomas <mgthomas0616@gmail.com>
2020-11-01 18:45:44 -0600

    starting on about me page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar.js";
+import LandingPage from "./components/LandingPage.js";
+import About from "./components/About/About.js";
 
+import { Route, Switch } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={LandingPage} />
+        <Route exact path="/about" component={About} />
+
+        <LandingPage />
+      </Switch>
     </div>
   );
 }
